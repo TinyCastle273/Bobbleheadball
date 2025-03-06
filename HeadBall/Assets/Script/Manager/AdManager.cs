b57be4492ccdb38711b880096fb502ca44be880e
cuongnguyen-2002 <nguyenduccuong.072002@gmail.com>
2025-03-06 09:06:35 +0700

    remove distribution, Add GamePix

--- HeadBall/Assets/Script/Manager/AdManager.cs
+++ HeadBall/Assets/Script/Manager/AdManager.cs
@@ -2,91 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using GamePix;
 public class AdManager : SingletonMono<AdManager>
 {
-    private Action _onRewardSuccess;
-    private Action _onRewardFailed;
-    private void Start()
-    {
-        GameDistribution.Instance.PreloadRewardedAd();
-    }
-
-    void Awake()
-    {
-        GameDistribution.OnResumeGame += OnResumeGame;
-        GameDistribution.OnPauseGame += OnPauseGame;
-        GameDistribution.OnPreloadRewardedVideo += OnPreloadRewardedVideo;
-        GameDistribution.OnRewardedVideoSuccess += OnRewardedVideoSuccess;
-        GameDistribution.OnRewardedVideoFailure += OnRewardedVideoFailure;
-        GameDistribution.OnRewardGame += OnRewardGame;
-    }
-
-    public void OnResumeGame()
-    {
-        // RESUME MY GAME
-        AudioManager.Instance.EnableSound(true);
-        ScenesController.Instance.PauseGame(false);
-    }
-
-    public void OnPauseGame()
-    {
-        // PAUSE MY GAME
-        AudioManager.Instance.EnableSound(false);
-        ScenesController.Instance.PauseGame(true);
-    }
-
-    public void OnRewardGame()
-    {
-        // REWARD PLAYER HERE
-        AudioManager.Instance.EnableSound(true);
-        this._onRewardSuccess?.Invoke();
-        this._onRewardSuccess = null;
-
-    }
-
-    public void OnRewardedVideoSuccess()
-    {
-        // Rewarded video succeeded/completed.;
-        AudioManager.Instance.EnableSound(true);
-
-    }
-
-    public void OnRewardedVideoFailure()
-    {
-        // Rewarded video failed.;
-        AudioManager.Instance.EnableSound(true);
-        this._onRewardFailed?.Invoke();
-        this._onRewardFailed = null;
-    }
-
-    public void OnPreloadRewardedVideo(int loaded)
-    {
-        // Feedback about preloading ad after called GameDistribution.Instance.PreloadRewardedAd
-        // 0: SDK couldn't preload ad
-        // 1: SDK preloaded ad
-    }
-
     public void ShowAd()
     {
-        AudioManager.Instance.EnableSound(false);
-        GameDistribution.Instance.ShowAd();
-#if UNITY_EDITOR
-        Debug.Log("Show Ad");
-#endif
+        Gpx.Ads.InterstitialAd(OnInterstitalAdSuccess);
     }
 
-    public void ShowRewardedAd(Action success, Action failed = null)
+    [AOT.MonoPInvokeCallback(typeof(Gpx.gpxCallback))]
+    public void OnInterstitalAdSuccess()
     {
-        AudioManager.Instance.EnableSound(false);
-        this._onRewardFailed = failed;
-        this._onRewardSuccess = success;
-        this.PreloadRewardedAd();
-        GameDistribution.Instance.ShowRewardedAd();
+        Gpx.Log("SUCCESS");
     }
 
-    public void PreloadRewardedAd()
-    {
-        GameDistribution.Instance.PreloadRewardedAd();
-    }
 }
